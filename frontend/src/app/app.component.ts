6ca5e6a866689b2fcb9699a3afd71a4dbfff6a20
rajranjan672 <76998014+rajranjan672@users.noreply.github.com>
2025-01-12 19:33:45 +0530

    sdgsd
    sdf

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'frontend';
+  constructor(private router: Router) {}
+
+  // Navigate to login page
+  navigateToLogin(): void {
+    this.router.navigate(['/login']);
+  }
+
+  // Navigate to register page
+  navigateToRegister(): void {
+    this.router.navigate(['/register']);
+  }
 }
